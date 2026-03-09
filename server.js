0836cfbed3981f89b903b7366b47bed4e2d7a0f1
Aashu Kumawat <kumawataashu09@gmail.com>
2026-03-09 17:56:51 +0530

    change

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("Server is on root.");
 })
-const hostname = '0.0.0.0'; // Listen on all network interfaces
+const hostname = '160.187.54.224'; // Listen on all network interfaces
 
 app.listen(port, hostname, () => {
     console.log(`Server is running at http://${hostname}:${port}/`);
